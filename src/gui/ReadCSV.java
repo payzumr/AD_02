ddd64da7226bf4e051caed1cad4c640d30aaa154
Philipp K <philipp.kloth@haw-hamburg.de>
2013-05-07 00:23:08 +0200

    readCSV gecleant

--- src/gui/ReadCSV.java
+++ src/gui/ReadCSV.java
@@ -72,40 +72,6 @@ public class ReadCSV {
 		tmpFile.close();
 	}
 
-	// generiert eine Liste mit der vorgegeben lagergröße
-	public void writeItems() throws IOException {
-
-		File outfile = new File(liste.get(4).split("=")[1].trim());
-
-		int maxSize;
-
-		int id = 1;
-
-		// Schreibt die items in die csv datei
-		try (PrintWriter erzeugteDatei = new PrintWriter(new BufferedWriter(
-				new FileWriter(outfile)))) {
-			erzeugteDatei.println("item_id" + ";" + "productPosX" + ";"
-					+ "productPosY" + ";" + "productSize");
-
-			for (int y = 0; y < Simulation.N - 1; y++) {
-				for (int x = 0; x < Simulation.N; x++) {
-
-					if ((x >= Simulation.N - Simulation.NUMBOXINGPLANTS)
-							&& y == Simulation.N - 1) {
-						break;
-					}
-
-					maxSize = (int) ((Math.random()) * Simulation.ORDERMAXSIZE + 1);
-
-					erzeugteDatei.println(id + ";" + x + ";" + y + ";"
-							+ maxSize);
-					id++;
-
-				}
-			}
-		}
-	}
-
 	// Item liste einlesen
 	public List<Item> readItems() throws IOException {
 
@@ -180,8 +146,6 @@ public class ReadCSV {
                         gewichtgesamt += anItem.size() * listpairs.get(i + 1).get(j);
                     }
                 }
-		
-			
 		}
 			if(gewichtgesamt > Simulation.ORDERMAXSIZE){
 				//Hier wird "ein Auftrag entfernt" wenn das Gewicht zu hoch ist
@@ -212,118 +176,6 @@ public class ReadCSV {
                 }
 			}
 		}
-		
-		
-		//Jonas und Philipp reworked ende
-
-		// solange order "pro" robot noch nicht abgearbeitet wurde
-//		while (!orderComplete) {
-//
-//			countSameItem = 0;
-//
-//			// counter = anzahl an items die aus der orderliste abgearbeitet
-//			// wurden sind.
-//			// wenn ich nicht beim item letzten item bin....
-//			if (counter < (orderArr.size())) {
-//
-//				// holt die zeile der orderliste (nächstes item)
-//				zeile = (String) orderArr.get(counter);
-//				// hollt die Item id und die menge in tmp variable
-//				tmpId = Integer.parseInt(zeile.split(";")[0]);
-//				menge = Integer.parseInt(zeile.split(";")[1]);
-//				int gesamtgewicht = 0;
-//				
-//				for(int i = 0;i<item.size();i++){
-//					if(item.get(i).id() == tmpId){
-//						gesamtgewicht += item.get(i).size()*menge;
-//					}
-//				}
-//				
-//				
-//				
-//				
-//
-//				// Solange das selbe item noch eine menge hat und in kapazität
-//				// noch gewicht passt...
-//				for (int i = 0; i < menge; i++) {
-//
-//					// items durch laufen
-//					for (Item element : item) {
-//
-//						// überprüft item id vom jetzigen mit der von der Item
-//						// liste
-//						if (element.id() == tmpId) {
-//							// speicher elemet (das item ) in die tmp
-//							tempItem = element;
-//							this.itemSet.add(element);
-//							// gewicht zu addieren
-//							gw = gw + tempItem.size();
-//							// wie oft wurde das selbe item abgearbeitet.
-//							countSameItem++;
-//							// verlässt die schleifen wenn item gefunden
-//							break;
-//						}
-//					}
-//
-//					//falls das selbe item nicht mehr rein passt
-//					// dann..
-//					if ((gw + tempItem.size()) > currentMaxSize) {
-//						// entfernt item aus mit der Alten menge und fügt das
-//						// item mit der neuen menge hinzu
-//						orderArr.remove(counter);
-//
-//						orderArr.add(counter, tmpId + ";"
-//								+ (menge - countSameItem));
-//						retMap.put(tempItem, countSameItem);
-//						if ((menge - countSameItem) == 0) {
-//							// counter zählen um nächstes item zu hollen
-//							counter++;
-//						}
-//						orderComplete = true;
-//						break;
-//					}
-//
-//				}
-//
-//			
-//				if (!orderComplete) {
-//					// aktuelle item hat komplett gepasst und wird in die orderMap
-//					// gespeichert
-//					retMap.put(tempItem, menge);
-//
-//					int i = counter + 1;
-//					counter++;
-//
-//					// überprüfe ob das nächste item noch vom robot zu tragen
-//					// wäre, ansonsten return die orderMap
-//					if (i < orderArr.size()) {
-//						zeile = (String) orderArr.get(i);
-//						tmpId = Integer.parseInt(zeile.split(";")[0]);
-//
-//						for (Item element : item) {
-//
-//							if (element.id() == tmpId) {
-//								tempItem = element;
-//								break;
-//							}
-//						}
-//
-//						if ((gw + tempItem.size()) > currentMaxSize) {
-//							orderComplete = true;
-//						}
-//					} else {
-//						orderComplete = true;
-//					}
-//
-//				}
-//
-//			} else {
-//				orderComplete = true;
-//			}
-//
-//		}
-		//retMap.clear();
-		//Edited by j and p
 		List<TreeMap<Item,Integer>> retmap = new ArrayList<>();
 		for(int i = 0;i<listpairs.size();i=i+2){
 			TreeMap<Item,Integer> tmp = new TreeMap<>();
